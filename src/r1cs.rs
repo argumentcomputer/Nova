10d30a6f7243d49c9aee6e376c3c8f72ef28bb5e
Hanting Zhang <hantingz@usc.edu>
2023-06-08 16:42:34 -0700

    use srinath trick

--- src/r1cs.rs
+++ src/r1cs.rs
@@ -72,7 +72,7 @@ impl<G: Group> R1CS<G> {
   pub fn commitment_key(S: &R1CSShape<G>) -> CommitmentKey<G> {
     let num_cons = S.num_cons;
     let num_vars = S.num_vars;
-    let total_nz = S.A.len() + S.B.len() + S.C.len();
+    let total_nz = 0; // S.A.len() + S.B.len() + S.C.len();
     G::CE::setup(b"ck", max(max(num_cons, num_vars), total_nz))
   }
 }
